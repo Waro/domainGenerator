f2d5e8863833b79ad9f18dade4789dabab304160
Waro <waro.brambilla@gmail.com>
2022-06-17 19:37:29 +0000

    app.js

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ function domainNameGenerator() {
         for (j=0; j<adj.lenght; j++){
             for(n=0; n<noun.lenght; n++){
                 for( t=0; t<ext.lenght; t++) {
-                    console.log('${pronoun[i]}${adj[j]}${noun[n]}${ext[t]}')     
+                    console.log('${pronoun[i]}${adj[j]}${noun[n]}${ext[t]}')   
                 }
             }
         }
     }
 }
-
-domainNameGenerator(pronoun,adj,noun,ext)
+domainNameGenerator()
